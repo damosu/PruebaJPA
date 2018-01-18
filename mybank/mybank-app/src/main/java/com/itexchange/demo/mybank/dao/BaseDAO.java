573b126cf6b4528b84b0885c92c91e0f9def7c49
David Borja <alvaro.borja@capgemini.com>
2018-01-18 14:01:25 -0500

    Uploading database scripts to complain H2 syntax

--- mybank/mybank-app/src/main/java/com/itexchange/demo/mybank/dao/BaseDAO.java
+++ mybank/mybank-app/src/main/java/com/itexchange/demo/mybank/dao/BaseDAO.java
@@ -4,8 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 public abstract class BaseDAO {
-
 	@PersistenceContext(unitName = "default")
 	protected EntityManager entityManager;
 
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
 }
